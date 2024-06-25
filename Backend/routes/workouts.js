f99a3cadba6e97dfef59d4d6ebc8d8e40ed96cc6
Ahmad Bin Rashid <ahmadbinrashid92@gmail.com>
2024-06-25 21:50:37 +0500

    Uploaded Files

--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -1,32 +1,22 @@
 
 const express = require('express')
 const Workout = require('../models/workoutModel')
-const {
-    createWorkout,
-} = require('../controllers/workoutController')
+const { getWorkouts, getWorkout, createWorkout, deleteWorkout, updateWorkout } = require('../controllers/workoutController')
 const router = express.Router()
 
 // get all workouts
-router.get('/', (req, res) => {
-    res.json({mssg: 'get all workouts'})
-})
+router.get('/', getWorkouts)
 
 // get a new workout
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'get a single workout'})
-})
+router.get('/:id', getWorkout)
 
 // post a new workout
 router.post('/', createWorkout)
 
 // delete a workout
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'delete a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
 // update a workout
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'update a workout'})
-})
+router.patch('/:id', updateWorkout)
 
 module.exports = router
