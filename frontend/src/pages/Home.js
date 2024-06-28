5dc82229b6924b2ac7b2182fcd13385133bdc756
Ahmad Bin Rashid <ahmadbinrashid92@gmail.com>
2024-06-29 00:00:48 +0500

    Uploaded Files

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react"
-
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
 
-    const [workouts, setWorkouts] = useState(null)
+    const { workouts, dispatch } = useWorkoutsContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -13,7 +13,7 @@ const Home = () => {
             const json = await response.json()
 
             if (response.ok) {
-                setWorkouts(json)
+                dispatch({type: 'SET-WORKOUTS', payload: json})
             }
         }
 
