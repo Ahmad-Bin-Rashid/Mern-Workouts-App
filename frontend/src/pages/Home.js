422943059ae3df0898c019eba03b1a1570d70998
Ahmad Bin Rashid <ahmadbinrashid92@gmail.com>
2024-07-23 22:21:23 +0500

    Correcting Mistakes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 
-// componenets
+// components
 import WorkoutDetails from "../components/WorkoutDetails"
 import WorkoutForm from "../components/WorkoutForm"
 
@@ -15,19 +15,18 @@ const Home = () => {
             const json = await response.json()
 
             if (response.ok) {
-                console.log(json)
-                dispatch({type: 'SET-WORKOUTS', payload: json})
+                dispatch({ type: 'SET_WORKOUTS', payload: json })
             }
         }
 
         fetchWorkouts()
     }, [dispatch])
-    
+
     return (
         <div className="home">
             <div className="workouts">
                 {workouts && workouts.map(workout => (
-                <WorkoutDetails workout={workout} key={workout._id} />
+                    <WorkoutDetails workout={workout} key={workout._id} />
                 ))}
             </div>
             <WorkoutForm />
